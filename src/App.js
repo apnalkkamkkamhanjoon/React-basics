5c8e6592372a346502a5da8754cda33253db25c4
cutejoon <s22073@gsm.hs.kr>
2023-02-07 14:57:55 +0900

    JSX

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   const style = {
     backgroundColor: 'black',
     color: 'aqua',
-    fontSize: 24, // 기본 단위 px
-    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+    fontSize: 24,
+    padding: '1rem'
   }
 
   return (
